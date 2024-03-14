b05824ed42ae2d225f066b5ed78d6ac0dd6e69cb
ecureuill <logika.sciuro@gmail.com>
2024-03-14 16:28:28 -0300

    Create brach for allure report

--- cypress/support/login.commands.js
+++
@@ -1,8 +0,0 @@
-Cypress.Commands.add('login', (user) => {
-  cy.request({
-    url: '/login',
-    method: 'POST',
-    body: user,
-    failOnStatusCode: false
-  }).then((response) => response);
-})
